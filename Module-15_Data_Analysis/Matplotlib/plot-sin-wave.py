3b61321651353e0845aa83196c80bc628f52c2a2
NovatecStack <121426292+novatecstack@users.noreply.github.com>
2023-09-17 21:24:29 +0530

    Create plot-sin-wave.py

---
+++ Module-15_Data_Analysis/Matplotlib/plot-sin-wave.py
@@ -0,0 +1,33 @@
+# Description: Python program to plot a 'Sine' wave using Matplotlib and Numpy
+
+# Sine waves represent periodic oscillations and has a shape of sine curve.
+# The X-axis of the sine curve represents the time.
+# The Y-axis of the sine curve represents the amplitude of the sine wave.
+
+import numpy as np
+import matplotlib.pyplot as plot
+
+# Get x values of the sine wave
+time = np.arange(0, 10, 0.1);
+
+# Amplitude of the sine wave is sine of a variable like time
+amplitude   = np.sin(time)
+
+# Plot a sine wave using time and amplitude obtained for the sine wave
+plot.plot(time, amplitude)
+
+# Give a title for the sine wave plot
+plot.title('Sine wave')
+
+# Give x axis label for the sine wave plot
+plot.xlabel('Time')
+
+# Give y axis label for the sine wave plot
+plot.ylabel('Amplitude = sin(time)')
+
+plot.grid(True, which='both')
+plot.axhline(y=0, color='k')
+plot.show()
+
+# Display the sine wave
+plot.show()
