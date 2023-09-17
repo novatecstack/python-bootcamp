8672af17339c433e16e0cf4720f2ed9f02afa787
NovatecStack <121426292+novatecstack@users.noreply.github.com>
2023-09-17 21:28:58 +0530

    Update plot-sin-wave.py

--- Module-15_Data_Analysis/Matplotlib/plot-sin-wave.py
+++ Module-15_Data_Analysis/Matplotlib/plot-sin-wave.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plot
 
 # Get x values of the sine wave
-time = np.arange(0, 10, 0.1);
+time = np.arange(0, 10, 0.1)
 
 # Amplitude of the sine wave is sine of a variable like time
 amplitude   = np.sin(time)
